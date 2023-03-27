ed387375abed0b197f5c665ca57ea608742211a8
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-03-27 21:48:05 +0600

    Added Products

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+    console.log(props.product.name)
+    const { img, name, seller, quantity, price, ratings } = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: ${seller}</p>
+                <p> Rating: {ratings} stars</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
